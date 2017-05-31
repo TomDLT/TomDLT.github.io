4203ce392da5bc34cdabaee12e57f828367c37e6
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2017-05-31 19:35:39 +0200

    change layout

--- script.js
+++ script.js
@@ -5,47 +5,7 @@ function change_tab(new_tab)
         document.getElementById('tab_content_'+current_tab).style.display = 'none';
         document.getElementById('tab_content_'+new_tab).style.display = 'block';
         current_tab = new_tab;
-        details(false);
 }
 
-var current_tab = 'publications';
+var current_tab = 'experience';
 change_tab(current_tab);
-var show_details = false;
-details(false);
-
-
-function len(tab)
-{
-    switch(tab) {
-    case 'experience':
-        return 4
-    case 'education':
-        return 3
-    case 'software':
-        return 2
-    default:
-        return 0
-}
-}
-
-function details(change)
-{
-    if (show_details == change)
-    {
-        show_details = false;
-        document.getElementById('details_button').innerHTML = 'Show details';
-        for (i = 0; i < len(current_tab); i++)
-        {
-            document.getElementById('details_'+current_tab+i).style.display = 'none'
-        }
-    }
-    else
-    {
-        show_details = true;
-        document.getElementById('details_button').innerHTML = 'Hide details';
-        for (i = 0; i < len(current_tab); i++)
-        {
-            document.getElementById('details_'+current_tab+i).style.display = 'block'
-        }
-    }
-}
