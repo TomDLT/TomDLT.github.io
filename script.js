eaf17de808e8bf08b944413dc940df3fb4ab2049
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2017-06-14 16:35:19 +0200

    update tab hash

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function change_tab(new_tab)
         document.getElementById('tab_'+new_tab).className = 'tab_on tab';
         document.getElementById('tab_content_'+current_tab).style.display = 'none';
         document.getElementById('tab_content_'+new_tab).style.display = 'block';
+        window.location.hash = new_tab;
         current_tab = new_tab;
         return false;
 }
 
 var current_tab = 'publications';
 var hash = window.location.hash.substr(1);
-debugger;
+
 if (hash == '')
     change_tab(current_tab);
 else
