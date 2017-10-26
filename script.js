82742d9cc7437e2c3c8ff4fbb166bf44296db5df
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2017-10-26 18:05:21 +0200

    add nbsp, and fix tab_on color

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function change_tab(new_tab)
 {
         document.getElementById('tab_'+current_tab).className = 'tab_off tab';
         document.getElementById('tab_'+new_tab).className = 'tab_on tab';
+        document.getElementById('tab2_'+current_tab).className = 'tab_off tab';
+        document.getElementById('tab2_'+new_tab).className = 'tab_on tab';
         document.getElementById('tab_content_'+current_tab).style.display = 'none';
         document.getElementById('tab_content_'+new_tab).style.display = 'block';
         window.location.hash = new_tab;
